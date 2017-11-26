525b33afa0aae5ff31a079b9bb332d86054057c6
aleks_rdvn <aleks.rdvn@gmail.com>
2017-11-26 03:46:57 +0100

    test POST /todos route

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+module.exports = { app };
