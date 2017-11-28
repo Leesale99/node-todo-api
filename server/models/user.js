24cc2cba4b15c2950b87114f71ad29ce315bba6f
aleks_rdvn <aleks.rdvn@gmail.com>
2017-11-28 19:50:22 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,24 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return bcrypt.compare(password, user.password).then(result => {
+      if (!result) {
+        return Promise.reject();
+      }
+
+      return user;
+    });
+  });
+};
+
 UserSchema.pre('save', function(next) {
   const user = this;
 
